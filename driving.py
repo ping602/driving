799dbd17f36c2949b8aaf55d7614b793a7c7744f
ping602 <k97049@gmail.com>
2022-03-13 14:09:30 +0800

    add US

--- driving.py
+++ driving.py
@@ -4,5 +4,10 @@ age = int(age)
 if country =='台灣':
     if age >= 18:
         print('你可以考駕照')
-    eles:
+    else:
+        print('你還不能考駕照')
+elif country =='美國':
+	if age >= 16:
+        print('你可以考駕照')
+    else:
         print('你還不能考駕照')
